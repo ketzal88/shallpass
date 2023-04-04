28a75a3b150d33fcd9888f5cbe4b9c3865913d47
clod2008 <claudio@apsis.com.ar>
2023-04-04 12:05:59 -0300

    ajuste responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,16 +15,17 @@ export const Header = ({ logo }) => {
         id='logo'
         xs={12}
         md={12}
+        sm={12}
         lg={6}
         xl={6}
       >
         <Image src={logo} />
         <Col>
           {/* Visible only on sm	.d-none .d-sm-block .d-md-none */}
-          <h1 className='d-block d-sm-none'>
+          <h1 className='d-block d-lg-none'>
             Nos especializamos en la atención de <span>Comercios, Emprendedores y PyMES.</span>
           </h1>
-          <h1 className='d-none d-sm-block'>
+          <h1 className='d-none d-lg-block'>
             Nos especializamos en<br /> la atención de<br /> <span>Comercios,<br /> Emprendedores y<br /> PyMES.</span>
           </h1>
         </Col>
