8d07582caafae4acc48dcf0823edb8d1cf5f0db8
Claudio Uccello <claudio@apsis.com.ar>
2024-02-13 18:19:48 -0300

    playsinline

--- src/components/HerroVideoBanner/HerroVideoBanner.js
+++ src/components/HerroVideoBanner/HerroVideoBanner.js
@@ -6,35 +6,40 @@ import { vectorCircle } from '../../assets'
 import styles from './HerroVideoBanner.module.scss'
 import useScreenSize from '../../hooks/useScreenSize'
 
-export const HerroVideoBanner = ({video, videoMp4, poster}) => {
+export const HerroVideoBanner = ({ video, videoMp4, poster }) => {
 
     const isMobile = useScreenSize();
 
-  return (
-    <Container className={`${styles.HerroVideoBanner}`}>
-          <Row className='h-100 align-items-center' style={{margin: '0px'}}>
-            <Col className={`${styles.heroText}`} md={{ span: 10, offset: 1 }}>
-                <small>Internacionalizá tus negocios</small>
-                <h1>ARMÁ {isMobile && <br />}TU LLC <br/>EN <span>3 DÍAS</span></h1>
-                <h4>Creá una cuenta bancaria en <span>USA y {!isMobile && <br/>}cobrá en dolares.</span></h4>
-                <Col className={`${styles.ctaContainer}  h-100 `}>
-                    <Col className='d-flex'>
-                        <Button className="rounded-pill">
-                            Enviar consulta
-                        </Button>
-                        <Image className={`${styles.vectorColor}`} src={vectorCircle} />
-                    </Col>    
+    return (
+        <Container className={`${styles.HerroVideoBanner}`}>
+            <Row className='h-100 align-items-center' style={{ margin: '0px' }}>
+                <Col className={`${styles.heroText}`} md={{ span: 10, offset: 1 }}>
+                    <small>Internacionalizá tus negocios</small>
+                    <h1>ARMÁ {isMobile && <br />}TU LLC <br />EN <span>3 DÍAS</span></h1>
+                    <h4>Creá una cuenta bancaria en <span>USA y {!isMobile && <br />}cobrá en dolares.</span></h4>
+                    <Col className={`${styles.ctaContainer}  h-100 `}>
+                        <Col className='d-flex'>
+                            <Button className="rounded-pill">
+                                Enviar consulta
+                            </Button>
+                            <Image className={`${styles.vectorColor}`} src={vectorCircle} />
+                        </Col>
+                    </Col>
                 </Col>
-            </Col>
-            <Col className={`${styles.videoBack}`}>
-                {/* <video src={video} className={`${styles.videoStyle}`} muted autoPlay loop /> */}
-                  <video className={`${styles.videoStyle}`} muted autoPlay loop poster={poster}>
-                      <source src={video} type="video/webm" />
-                      <source src={videoMp4} type="video/mp4" />
-                      Tu navegador no soporta el elemento de video.
-                </video>
-            </Col>
-        </Row>
-    </Container>
-  )
+                <Col className={`${styles.videoBack}`}>
+                    {/* <video src={video} className={`${styles.videoStyle}`} muted autoPlay loop /> */}
+                    <video className={`${styles.videoStyle}`} 
+                        playsinline 
+                        muted 
+                        autoPlay 
+                        loop 
+                        poster={poster}>
+                        <source src={video} type="video/webm" />
+                        <source src={videoMp4} type="video/mp4" />
+                        Tu navegador no soporta el elemento de video.
+                    </video>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
