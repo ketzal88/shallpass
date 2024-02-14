ab8ff47cc6ae5c6d7c50d598361ab1ab6607c1bc
Claudio Uccello <claudio@apsis.com.ar>
2024-02-14 19:35:30 -0300

    THANK YOU PAGE

--- src/pages/Thanks.js
+++ src/pages/Thanks.js
@@ -1,22 +1,35 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Col, Row, Container, Image, Button } from "react-bootstrap";
+import { useNavigate } from 'react-router-dom';
 import "./Thanks.scss";
+import { logoPassFullColor } from "../assets";
 
 export const Thanks = () => {
+
+  const navigate = useNavigate();
+
   return (
-    <Row id="thanks">
-      <Col xs={12} className="d-flex justify-content-center">
-        <h1>
-          Gracias por contactanos.
-          <br /> <span>Nos comunicaremos a la brevedad</span>
-        </h1>
-      </Col>
-      <Col xs={12} className="mt-5">
-        <Link to={"/"}>
-          <p>Volver al home</p>
-        </Link>
-      </Col>
-    </Row>
+    <Container id="thanks" fluid>
+      <Row className="logoContainer">
+        <Col className="logoBox">
+          <Image src={logoPassFullColor} fluid />
+        </Col>
+      </Row>
+      <Row className="mesageContainer">
+        <Col xs={12} className="col align-self-center">
+          <h1>
+            Muchas gracias!
+            <br />
+          </h1>
+          <p>Hemos enviado su consulta con éxito</p>
+          <Button
+            className="rounded-pill"
+            onClick={() => navigate(-1)}
+          >
+            Volver a la página anterior
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
