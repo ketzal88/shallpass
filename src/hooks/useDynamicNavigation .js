11160d96fa75ae0cfe99e10d34ae4989b95b77fa
Gabriel Uccello <gabrielucc@gmail.com>
2024-02-15 16:33:24 -0300

    evntyo whatsapp

--- src/hooks/useDynamicNavigation .js
+++ src/hooks/useDynamicNavigation .js
@@ -1,23 +1,32 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export const useDynamicNavigation = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const whatsAppNumber = '+5491134658988';
-    const whatsAppData = `https://api.whatsapp.com/send?phone=${whatsAppNumber}&text=Hola!%20Quisiera%20consultar%20por%20sus%20servicios.`;
+  const handleWhatsAppClick = () => {
+    window.dataLayer.push({
+      event: "WhatsApp",
+      sent: true,
+    });
+  };
 
-    // Función para manejar el click, acepta un objeto como argumento
-    const handleNavigation = ({ url = whatsAppData, newTab = false } = {}) => {
-        return () => {
-            if (newTab) {
-                // Abrir en una nueva pestaña
-                window.open(url, '_blank');
-            } else {
-                // Navegación interna normal con React Router
-                navigate(url, { replace: true });
-            }
-        };
+  const whatsAppNumber = "+5491134658988";
+  const whatsAppData = `https://api.whatsapp.com/send?phone=${whatsAppNumber}&text=Hola!%20Quisiera%20consultar%20por%20sus%20servicios.`;
+
+  // Función para manejar el click, acepta un objeto como argumento
+  const handleNavigation = ({ url = whatsAppData, newTab = false } = {}) => {
+    return () => {
+      if (newTab) {
+        handleWhatsAppClick();
+        // Abrir en una nueva pestaña
+        window.open(url, "_blank");
+      } else {
+        handleWhatsAppClick();
+        // Navegación interna normal con React Router
+        navigate(url, { replace: true });
+      }
     };
+  };
 
-    return handleNavigation;
+  return handleNavigation;
 };
