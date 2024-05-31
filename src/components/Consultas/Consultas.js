1c47bdd4329f2a032f4efe9089f7b27e3fa536d8
Claudio Uccello <claudio@apsis.com.ar>
2024-05-31 15:39:22 -0300

    consultas ok

--- src/components/Consultas/Consultas.js
+++ src/components/Consultas/Consultas.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Button, Col, Container, Image, Row } from 'react-bootstrap'
-import { useDynamicNavigation } from '../../hooks/useDynamicNavigation '
+// import { useDynamicNavigation } from '../../hooks/useDynamicNavigation '
 import { vector } from '../../assets'
 import styles from './Consultas.module.scss'
+import useScrollTo from '../../hooks/useScrollTo'
 
 export const Consultas = () => {
 
-  const navigate = useDynamicNavigation({ subject: `${process.env.REACT_APP_LLC_SUBJECT}` });
+  // const navigate = useDynamicNavigation({ subject: `${process.env.REACT_APP_LLC_SUBJECT}` });
+
+
+  const scrollTo = useScrollTo();
 
   return (
     <Container className={`${styles.consultas}`}>
@@ -19,7 +23,8 @@ export const Consultas = () => {
           <h3>¿Querés hacer una <span>CONSULTA GRATUITA?</span></h3>
           <Button
             className={`rounded-pill`}
-            onClick={navigate({ newTab: true })}
+            // onClick={navigate({ newTab: true })}
+            onClick={() => scrollTo('llc')}
           >
             Consulta gratis <Image src={vector} width={24} height={24} />
           </Button>
