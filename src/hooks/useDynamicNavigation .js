6b46460eac1ff8b69f2e51a0b4c9a60405000299
Claudio Uccello <claudio@apsis.com.ar>
2024-06-05 18:31:08 -0300

    dinamicNav

--- src/hooks/useDynamicNavigation .js
+++ src/hooks/useDynamicNavigation .js
@@ -1,27 +1,18 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
-export const useDynamicNavigation = () => {
+export const useDynamicNavigation = ({ number = '+5491134658988', subject = `${process.env.REACT_APP_HOME_SUBJECT}` } = {}) => {
   const navigate = useNavigate();
 
-  const handleWhatsAppClick = () => {
-    window.dataLayer.push({
-      event: "WhatsApp",
-      sent: true,
-    });
-  };
-
-  const whatsAppNumber = "+5491134658988";
-  const whatsAppData = `https://api.whatsapp.com/send?phone=${whatsAppNumber}&text=Hola!%20Quisiera%20consultar%20por%20sus%20servicios.`;
+  // Construir la URL de WhatsApp
+  const whatsAppData = `https://api.whatsapp.com/send?phone=${encodeURIComponent(number)}&text=${encodeURIComponent(subject)}`;
 
   // Función para manejar el click, acepta un objeto como argumento
   const handleNavigation = ({ url = whatsAppData, newTab = false } = {}) => {
     return () => {
       if (newTab) {
-        handleWhatsAppClick();
         // Abrir en una nueva pestaña
-        window.open(url, "_blank");
+        window.open(url, '_blank');
       } else {
-        handleWhatsAppClick();
         // Navegación interna normal con React Router
         navigate(url, { replace: true });
       }
