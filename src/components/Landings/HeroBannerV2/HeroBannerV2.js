159b29104d06d10afdbfbff89701cd21577af39f
Claudio Uccello <claudio@apsis.com.ar>
2024-06-06 15:37:51 -0300

    test heroBanner 2

---
+++ src/components/Landings/HeroBannerV2/HeroBannerV2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './HeroBannerV2.module.scss'
+
+export const HeroBannerV2 = () => {
+    return (
+        <div className={`${styles.main}`}>HeroBannerV2</div>
+    )
+}
