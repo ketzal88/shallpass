a89593c9f40a44c05e63e99d590efe2b9f58aa3e
Claudio Uccello <claudio@apsis.com.ar>
2024-06-27 14:50:09 -0300

    disabel button

--- src/components/Form/FormLandig.js
+++ src/components/Form/FormLandig.js
@@ -14,6 +14,7 @@ export const FormLanding = ({
 }) => {
   const form = useRef();
   const [formResponse, setFormResponse] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleFormDataLayer = (email, phone) => {
     window.dataLayer.push({
@@ -26,24 +27,28 @@ export const FormLanding = ({
 
   const sendEmail = (e) => {
     e.preventDefault();
-    emailjs
-      .sendForm(
-        `${SERVICE_ID}`,
-        `${TEMPLATE_ID}`,
-        form.current,
-        `${PUBLIC_KEY}`
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setFormResponse(true);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    if (!loading) {
+      setLoading(true);
+      emailjs
+        .sendForm(
+          `${SERVICE_ID}`,
+          `${TEMPLATE_ID}`,
+          form.current,
+          `${PUBLIC_KEY}`
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            setFormResponse(true);
+          },
+          (error) => {
+            console.log(error.text);
+            setLoading(false); // Enable the button again if there's an error
+          }
+        );
 
-    handleFormDataLayer(form.current[2].value, form.current[1].value);
+      handleFormDataLayer(form.current[2].value, form.current[1].value);
+    }
   };
 
   const navigate = useNavigate();
@@ -74,7 +79,7 @@ export const FormLanding = ({
             className={`${styles.itemForm}`}
             style={{ paddingRight: '12px', paddingLeft: '12px' }}
           >
-            <Button variant="primary" className="rounded-pill" type="submit">
+            <Button variant="primary" className="rounded-pill" type="submit" disabled={loading}>
               {submitText} <Image src={vector} />
             </Button>
           </Col>
