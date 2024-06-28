051443cc4cac67e5d6d748709a063515d908793b
Gabriel Uccello <gabrielucc@gmail.com>
2024-06-28 15:22:06 -0300

    tilde

--- src/components/FormSection/FormSection.js
+++ src/components/FormSection/FormSection.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
-import { FormLanding } from '../Form/FormLandig'
-import styles from './FormSection.module.scss'
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { FormLanding } from "../Form/FormLandig";
+import styles from "./FormSection.module.scss";
 
 export const FormSection = ({ submitText, id }) => {
-
-
-
   return (
     <Container className={`${styles.formSection}`}>
       <Row className={`justify-content-md-center h-100`}>
-        <Col lg={8} xl={6}
-
-        >
+        <Col lg={8} xl={6}>
           <Col>
             <h6>Confiá en shallpass</h6>
-            <h4>Déjanos tu email y un asesor de Shallpass te estará contactando<br /><span>a la brevedad</span></h4>
+            <h4>
+              Dejanos tu email y un asesor de Shallpass te estará contactando
+              <br />
+              <span>a la brevedad</span>
+            </h4>
           </Col>
-          <Col >
+          <Col>
             <FormLanding
               submitText={submitText}
               TEMPLATE_ID={process.env.REACT_APP_LANDING_TEMPLATE_ID}
@@ -27,5 +26,5 @@ export const FormSection = ({ submitText, id }) => {
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
