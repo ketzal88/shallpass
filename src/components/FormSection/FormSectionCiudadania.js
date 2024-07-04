905c5be10e864e93cb50b525dd2ed6c01d515e4b
Gabriel Uccello <gabrielucc@gmail.com>
2024-07-04 20:45:17 -0300

    cambios whatsapp ciudadanias

--- src/components/FormSection/FormSectionCiudadania.js
+++ src/components/FormSection/FormSectionCiudadania.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import { FormLanding } from "../Form/FormLandig";
+import { FormLandingWhatsApp } from "../Form/FormLandigWhatApp";
 import styles from "./FormSection.module.scss";
 
-export const FormSection = ({ submitText, id }) => {
+export const FormSectionCiudadania = ({ submitText, id }) => {
   return (
     <Container className={`${styles.formSection}`}>
       <Row className={`justify-content-md-center h-100`}>
         <Col lg={8} xl={6}>
           <Col>
             <h6>Confiá en shallpass</h6>
-            <h4>Dejá tu email y nos comunicaremos</h4>
+            <h4>Dejá tu WhatsApp y nos comunicaremos</h4>
           </Col>
           <Col>
-            <FormLanding
+            <FormLandingWhatsApp
               submitText={submitText}
               TEMPLATE_ID={process.env.REACT_APP_LANDING_TEMPLATE_ID}
               id={id}
--- src/pages/CiudadaniaEspanola.js
+++ src/pages/CiudadaniaEspanola.js
@@ -3,19 +3,19 @@ import { Atencion } from "../components/Landings/Atencion/Atencion";
 import { Col, Row } from "react-bootstrap";
 import { HeaderCTA } from "../components/Landings/HeaderCTA/HeaderCTA";
 import {
-    espanolHeroVideoMobileMp4,
-    espanolHeroVideoMp4,
-    posterEspanol,
+  espanolHeroVideoMobileMp4,
+  espanolHeroVideoMp4,
+  posterEspanol,
 } from "../assets";
 import { HighLigthText } from "../components/Landings/HighLigthText/HighLigthText";
 import { CardGrid } from "../components/Landings/CardGrid/CardGrid";
 import {
-    cardData,
-    expalinerStandarList,
-    explainer2StandarList,
-    explainerHigligthList,
-    explainerHigligthListTitle,
-    servicios,
+  cardData,
+  expalinerStandarList,
+  explainer2StandarList,
+  explainerHigligthList,
+  explainerHigligthListTitle,
+  servicios,
 } from "../data/ciudadaniaEspData";
 import { Explainer } from "../components/Landings/Explainer/Explainer";
 import { Explainer2 } from "../components/Landings/Explainer2/Explainer2";
@@ -23,76 +23,79 @@ import { HeroText } from "../components/Landings/HeroText/HeroText";
 import { CardGrid2 } from "../components/Landings/CardGrid2/CardGrid2";
 import { Price } from "../components/Landings/Price/Price";
 import { FooterLanding } from "../components/Landings/FooterLanding/FooterLanding";
-import { FormSection } from "../components/FormSection/FormSection";
+import { FormSectionCiudadania } from "../components/FormSection/FormSectionCiudadania";
 import useScreenSize from "../hooks/useScreenSize";
 
 import styles from "./Landings.module.scss";
 import { HeroBannerV2 } from "../components/Landings/HeroBannerV2/HeroBannerV2";
 
 export const CiudadaniaEspanola = () => {
-    const isMobile = useScreenSize();
+  const isMobile = useScreenSize();
 
-    const idPage = "espanola";
+  const idPage = "espanola";
 
-    return (
-        <>
-            <Row className={`justify-content-md-center ${styles.bgR}`}>
-                <Atencion />
-                <Col className={`${styles.main}`}>
-                    <HeaderCTA idTo={idPage} />
-                    <HeroBannerV2
-                        heroTex={`CONVERTITE <br/>en `}
-                        spanText={`Ciudadano <br/>Español`}
-                        smallText={`Abrazá un futuro de oportunidades`}
-                        explainer={`Estudiá, conseguí trabajo formal y disfrutá de la libertad de ingresar a otros países de la Unión Europea sin necesidad de tramitar una visa, entre otros beneficios.`}
-                        poster={posterEspanol}
-                        videoMp4={espanolHeroVideoMp4}
-                        videoMobileMp4={espanolHeroVideoMobileMp4}
-                        pageId={idPage}
-                    />
-                    <HighLigthText
-                        text={`Beneficios <br/>de obtener la <br/>ciudadanía <br/>española`}
-                    />
-                    <CardGrid cardData={cardData} />
-                </Col>
-            </Row>
-            <Row className={`justify-content-md-center ${styles.bgL}`}>
-                <Row
-                    className={`justify-content-md-center ${styles.explainerContainer}`}
-                >
-                    <Col className={`${styles.mainFullW}`}>
-                        <Explainer
-                            title={`Quiénes pueden solicitar la `}
-                            titleSpan={`ciudadanía española?`}
-                            standarList={expalinerStandarList}
-                            higligthListTitle={explainerHigligthListTitle}
-                            hihligthList={explainerHigligthList}
-                        />
-                    </Col>
-                </Row>
-                <Row
-                    className={`justify-content-md-center  ${styles.explainerContainer}`}
-                >
-                    <Col className={`${styles.mainFullW}`}>
-                        <Explainer2
-                            title={`Beneficios de tramitar tu carpeta con Shall Pass`}
-                            copy={`Obtener la ciudadanía española puede ser desafiante. Nosotros lo sabemos, por eso te vamos a asesorar durante todo el proceso.`}
-                            standarList={explainer2StandarList}
-                        />
-                    </Col>
-                </Row>
-                <Col className={`${styles.main}`}>
-                    <HeroText
-                        titel={`Servicio de armado de<br/>carpeta`}
-                        copy={`Con nuestro servicio vas a obtener`}
-                    />
-                    <CardGrid2 cardData={servicios} />
-                    <Price id={idPage} />
-                    <FormSection id={idPage} submitText={"Contactanos"} />
-                    {isMobile && <Col style={{ minHeight: "64px" }} />}
-                    <FooterLanding />
-                </Col>
-            </Row>
-        </>
-    );
+  return (
+    <>
+      <Row className={`justify-content-md-center ${styles.bgR}`}>
+        <Atencion />
+        <Col className={`${styles.main}`}>
+          <HeaderCTA idTo={idPage} />
+          <HeroBannerV2
+            heroTex={`CONVERTITE <br/>en `}
+            spanText={`Ciudadano <br/>Español`}
+            smallText={`Abrazá un futuro de oportunidades`}
+            explainer={`Estudiá, conseguí trabajo formal y disfrutá de la libertad de ingresar a otros países de la Unión Europea sin necesidad de tramitar una visa, entre otros beneficios.`}
+            poster={posterEspanol}
+            videoMp4={espanolHeroVideoMp4}
+            videoMobileMp4={espanolHeroVideoMobileMp4}
+            pageId={idPage}
+          />
+          <HighLigthText
+            text={`Beneficios <br/>de obtener la <br/>ciudadanía <br/>española`}
+          />
+          <CardGrid cardData={cardData} />
+        </Col>
+      </Row>
+      <Row className={`justify-content-md-center ${styles.bgL}`}>
+        <Row
+          className={`justify-content-md-center ${styles.explainerContainer}`}
+        >
+          <Col className={`${styles.mainFullW}`}>
+            <Explainer
+              title={`Quiénes pueden solicitar la `}
+              titleSpan={`ciudadanía española?`}
+              standarList={expalinerStandarList}
+              higligthListTitle={explainerHigligthListTitle}
+              hihligthList={explainerHigligthList}
+            />
+          </Col>
+        </Row>
+        <Row
+          className={`justify-content-md-center  ${styles.explainerContainer}`}
+        >
+          <Col className={`${styles.mainFullW}`}>
+            <Explainer2
+              title={`Beneficios de tramitar tu carpeta con Shall Pass`}
+              copy={`Obtener la ciudadanía española puede ser desafiante. Nosotros lo sabemos, por eso te vamos a asesorar durante todo el proceso.`}
+              standarList={explainer2StandarList}
+            />
+          </Col>
+        </Row>
+        <Col className={`${styles.main}`}>
+          <HeroText
+            titel={`Servicio de armado de<br/>carpeta`}
+            copy={`Con nuestro servicio vas a obtener`}
+          />
+          <CardGrid2 cardData={servicios} />
+          <Price id={idPage} />
+          <FormSectionCiudadania
+            id={idPage}
+            submitText={"Hablemos por WhatsApp"}
+          />
+          {isMobile && <Col style={{ minHeight: "64px" }} />}
+          <FooterLanding />
+        </Col>
+      </Row>
+    </>
+  );
 };
